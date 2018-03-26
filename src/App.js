cb71afef3af1a6bd74f3810f21292018b020baad
Wanchalerm <WANCHALERM.POPEE.Contractor@3ds.com>
2018-03-26 17:33:24 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:4001"
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
+  render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? <p>
+              {response.round}
+            </p>
+          : <p>Loading...</p>}
+          {response
+          ? <p>
+              {response.datetime}
+            </p>
+          : <p>Loading...</p>}
+            {response
+          ? <p>
+              {response.message}
+            </p>
+          : <p>Loading...</p>}
+            {response
+          ? <p>
+              {response.description}
+            </p>
+          : <p>Loading...</p>}
+      </div>
+    );
+  }
+}
+export default App;
